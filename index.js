b4dd957e1c936814f4cec4ce5c349c6aaa22a314
Timothy Cavinder <Timothycavinder@msn.com>
2019-03-23 13:05:13 -0400

    added functionality

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override')
+const flash = require('connect-flash')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+const mongoose = require('mongoose');
+
+const app = express();
+
+//passport config
+require('./config/passport')(passport);
+//DB config
+const db = require('./config/database');
+
+mongoose.connect(db.mongoURI, {
+    useMongoClient: true,
+    useNewUrlParser: true
+}) 
+.then(() => console.log('MongoDB Connected . . .'))
+.catch(err => console.log(err));
+
+
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    const title = 'Welcome Today';
+
+    res.render('index', {
+        title: title,
+        
+    });
+});
+
+
+
+const port = process.env.PORT || 5050;
+
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
